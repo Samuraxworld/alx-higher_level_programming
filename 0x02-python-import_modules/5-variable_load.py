3687e99e5b11b46d0c2d561fda081f53d4569c3a
Samuraxworld <Samuraxworld@gmail.com>
2023-06-08 11:23:44 -0700

    update

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """prints the value of variable a"""
+    """Print the value of variable."""
     from variable_load_5 import a
 
-    print (a)
+    print(a)
